465978682b0b9d8417ccd36b01d0ba9ca62cb4c7
Gabriel RABHI <gabriel.rabhi@gmail.com>
2026-05-08 20:48:53 +0200

    Fixes

---
+++ FractalSpecs.Agent.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using FractalSpecs.CodingAgent;
+using FractalSpecs.CodingAgent.Contracts;
+
+namespace FractalSpecs.Agent.Tests
+{
+    public class AgentShould
+    {
+        [Fact]
+        public void SendLMStudioPrompt()
+        {
+            var client = new TestAgentListenerClient();
+            var agent = new FractalSpecs.CodingAgent.Agent(new AgentOptions(), client);
+        }
+    }
+
+    public class TestAgentListenerClient : IAgentListenerClient
+    {
+        public void HistoryUpdated(CodingAgent.Agent agent, List<IAgentHistoryPart> newHistoryParts, bool end)
+        {
+
+        }
+    }
+}
--- FractalSpecs.CodingAgent/Outputs/TextAgentOutput.cs
+++ FractalSpecs.CodingAgent/Outputs/TextAgentOutput.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using FractalSpecs.CodingAgent.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FractalSpecs.Agent.Outputs
 {
-    public class TextAgentOutput : IAgentOutput
+    public class TextOutput : IAgentHistoryPart
     {
+        public TextOutput(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; }
     }
 }
